3209a1115d79419aef9079f784bc60b71816a895
danielruedaaa <daaniel.ruedaa@gmail.com>
2024-10-22 16:42:43 +0200

    Animacions jugador

--- Player.gd
+++ Player.gd
@@ -26,6 +26,17 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Player_ready():
+	hide()
